0960678e5fd4713f6c331ebe6f24526d649976e8
anto__dev <antonidwm@gmail.com>
2023-09-13 23:13:57 +0100

    ADD function change active state

--- portfolio2/src/components/AboutSectionVue.vue
+++ portfolio2/src/components/AboutSectionVue.vue
@@ -1,75 +1,76 @@
 <script setup>
 import SmallColInfoVue from "./SmallColInfo.vue";
 import BoxTitleHome from './BoxTitleHome.vue';
+import IconStarWhite1 from './icons/IconStarWhite1.vue';
 import { ref } from "vue";
 
 let allLinkAbout = [{ name: 'Design', actif: true }, { name: 'Frontend', actif: false }, { name: 'Logiciel?', actif: false }, { name: '+ sur moi', actif: false }];
 let arrayAbout = [
   {
-  title1: "Conception d'interface",
-  title2: "Conception de contenus graphiques",
-  allItem1: [
-    "Wireframes",
-    "Design",
-    "Atomic design",
-    "Responsive design",
-    "Respect UX & UI",
-    "..."
-  ], allItem2: [
-  "Icône / illustration SVG",
-    "Modifications d'images sur PS",
-    "Création d'images sur PS",
-    "Écriture de contenus"
-  ]
-}, {
-  title1: "Intégration pure",
-  title2: "Web app",
-  allItem1: [
-    "Intégration page web",
-    "SEO",
-    "Utilisation CMS",
-    "Du simple au complexe",
-    "..."
-  ], allItem2: [
-    "Création d'app web",
-    "Utilisation de frameWork",
-    "Vue / React / Svelte ...",
-    "Atomic components",
-    "..."
-  ]
-}, {
-  title1: "Des logiciels comme ...",
-  title2: "Des langages",
-  allItem1: [
-    "La suite adobe",
-    "Figma",
-    "Visual Studio Code ",
-    "Android Studio",
-    "..."
-  ], allItem2: [
-    "Js vanilla",
-    "React / Next / Vue / Svelte ",
-    "Php",
-    "Python & Kotlin en débutant",
-    "..."
-  ]
-}, {
-  title1: "Mes passions",
-  title2: "Des langages",
-  allItem1: [
-    "La musique & le piano",
-    "L'art & les illustrations",
-    "Les animaux & la nature ",
-    "Les jeux vidéos",
-    "..."
-  ], allItem2: [
-    "Créatif",
-    "Passionné  ",
-    "Persévérant",
-    "Toujours prêt à apprendre",
-    "..."
-  ]
-}]
+    title1: "Conception d'interface",
+    title2: "Conception de contenus graphiques",
+    allItem1: [
+      "Wireframes",
+      "Design",
+      "Atomic design",
+      "Responsive design",
+      "Respect UX & UI",
+      "..."
+    ], allItem2: [
+      "Icône / illustration SVG",
+      "Modifications d'images sur PS",
+      "Création d'images sur PS",
+      "Écriture de contenus"
+    ]
+  }, {
+    title1: "Intégration pure",
+    title2: "Web app",
+    allItem1: [
+      "Intégration page web",
+      "SEO",
+      "Utilisation CMS",
+      "Du simple au complexe",
+      "..."
+    ], allItem2: [
+      "Création d'app web",
+      "Utilisation de frameWork",
+      "Vue / React / Svelte ...",
+      "Atomic components",
+      "..."
+    ]
+  }, {
+    title1: "Des logiciels comme ...",
+    title2: "Des langages",
+    allItem1: [
+      "La suite adobe",
+      "Figma",
+      "Visual Studio Code ",
+      "Android Studio",
+      "..."
+    ], allItem2: [
+      "Js vanilla",
+      "React / Next / Vue / Svelte ",
+      "Php",
+      "Python & Kotlin en débutant",
+      "..."
+    ]
+  }, {
+    title1: "Mes passions",
+    title2: "Des langages",
+    allItem1: [
+      "La musique & le piano",
+      "L'art & les illustrations",
+      "Les animaux & la nature ",
+      "Les jeux vidéos",
+      "..."
+    ], allItem2: [
+      "Créatif",
+      "Passionné  ",
+      "Persévérant",
+      "Toujours prêt à apprendre",
+      "..."
+    ]
+  }]
 
 const itemData = ref(0)
 console.log(itemData.value);
@@ -86,6 +87,20 @@ function updateItemList(e) {
   else {
     itemData.value = 0
   }
+};
+
+function changeStateElNav(e) {
+ let allLinkek = document.querySelectorAll('.boxSLLink--allEL')
+ allLinkek.forEach((el)=>{
+      el.classList.remove('boxSLLink__el--actif');
+      
+    });
+    console.log(e);
+  if (e.target.classList == 'boxSLLink--allEL') {
+   
+    e.target.classList.add('boxSLLink__el--actif');
+    
+  }
 }
 </script>
 
@@ -94,13 +109,13 @@ function updateItemList(e) {
   <div class="sect__about">
     <div class="sect__topAbout">
       <div class="sect__boxSmallLLink" :style="{ background: '#ffffff' }">
-        <IconLongBoxStar />
+        <IconStarWhite1 :class="'topAbout__starYellow'" />
         <div class="boxSLLink__boxNav">
           <ul class="boxSLLink__list">
 
             <li class="boxSLLink__el" v-for="(item, index) in allLinkAbout" :key="index" v-on:click="updateItemList">
 
-              <p :class="[item.actif ? ' boxSLLink__el--actif ' : '']">
+              <p :class="[item.actif ? ' boxSLLink__el--actif boxSLLink--allEL ' : 'boxSLLink--allEL']" v-on:click="changeStateElNav">
                 {{ item.name }}
               </p>
             </li>
@@ -124,7 +139,7 @@ function updateItemList(e) {
             </h3>
             <ul class="topAbout__listItem">
               <li class="topAbout__elI" v-for="(item, index) in arrayAbout[itemData].allItem1" :key="index">
-               - {{ item }}
+                - {{ item }}
               </li>
             </ul>
           </li>
@@ -135,7 +150,7 @@ function updateItemList(e) {
             </h3>
             <ul class="topAbout__listItem">
               <li class="topAbout__elI" v-for="(item, index) in arrayAbout[itemData].allItem2" :key="index">
-               - {{ item }}
+                - {{ item }}
               </li>
             </ul>
           </li>
